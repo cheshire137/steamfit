2d11b538e4e16f11b1a7fe9dbebfa60bc4336e31
Sarah Vessels <cheshire137@gmail.com>
2015-12-31 18:35:09 -0500

    Pull in config details for a Fitbit sign in link

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import s from './HomePage.scss';
 import withStyles from '../../decorators/withStyles';
+import Config from '../config.json';
 
 const title = 'Connect with Fitbit';
 
@@ -16,11 +17,17 @@ class HomePage extends Component {
   }
 
   render() {
+    var authUrl = 'https://www.fitbit.com/oauth2/authorize' +
+                  '?client_id=' + Config.fitbit.clientId;
     return (
       <div className={s.root}>
         <div className={s.container}>
           <h1>{title}</h1>
-          <p></p>
+          <p>
+            <a className={s.link} href={authUrl}>
+              Sign in to Fitbit
+            </a>
+          </p>
         </div>
       </div>
     );
