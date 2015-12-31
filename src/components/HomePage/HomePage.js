97735fd786c7fa54efdfebafaadb2de4781b3e24
Sarah Vessels <cheshire137@gmail.com>
2015-12-31 18:45:54 -0500

    Flesh out Fitbit auth URL with state, other params

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import s from './HomePage.scss';
 import withStyles from '../../decorators/withStyles';
 import Config from '../config.json';
+import LocalStorage from '../LocalStorage';
 
 const title = 'Connect with Fitbit';
 
@@ -17,8 +18,16 @@ class HomePage extends Component {
   }
 
   render() {
+    var scopes = 'activity profile';
+    var state = new Date().getTime() / 1000;
+    LocalStorage.set('state', state);
     var authUrl = 'https://www.fitbit.com/oauth2/authorize' +
-                  '?client_id=' + Config.fitbit.clientId;
+                  '?client_id=' + Config.fitbit.clientId +
+                  '&response_type=token' +
+                  '&scope=' + encodeURIComponent(scopes) +
+                  '&redirect_uri=' +
+                  encodeURIComponent(Config.fitbit.redirectUri) +
+                  '&state=' + encodeURIComponent(state);
     return (
       <div className={s.root}>
         <div className={s.container}>
