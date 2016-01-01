0986262fbdf5e5ebcc53786e34e5f039fde5c07f
Sarah Vessels <cheshire137@gmail.com>
2016-01-01 12:25:59 -0500

    Store Steam username in local storage

--- src/components/FitbitPage/Steam.js
+++ src/components/FitbitPage/Steam.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import s from './FitbitPage.scss';
 import SteamUsernameForm from './SteamUsernameForm';
+import LocalStorage from '../../stores/localStorage';
 
 class Steam extends Component {
   constructor(props, context) {
@@ -8,16 +9,29 @@ class Steam extends Component {
     this.state = {};
   }
 
+  componentDidMount() {
+    this.setState({username: LocalStorage.get('steam')});
+  }
+
+  saveSteamUsername(username) {
+    this.setState({username: username});
+    if (typeof username === 'string') {
+      LocalStorage.set('steam', username);
+    } else {
+      LocalStorage.delete('steam');
+    }
+  }
+
   onSteamUsernameChange(username) {
     if (typeof username === 'string' && username.length < 1) {
       username = undefined;
     }
-    this.setState({username: username});
+    this.saveSteamUsername(username);
   }
 
   clearSteamUsername(event) {
     event.preventDefault();
-    this.setState({username: undefined});
+    this.saveSteamUsername(undefined);
   }
 
   render() {
@@ -26,7 +40,10 @@ class Steam extends Component {
       <div className={s.steam}>
         {hasUsername ? (
           <p>
-            Steam user name: {this.state.username}
+            Steam user name:
+            <span className={s.steamUsername}>
+              {this.state.username}
+            </span>
             <a href="#" className={s.clearSteamUsername} onClick={this.clearSteamUsername.bind(this)}>
               &times;
             </a>
