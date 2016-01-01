9b3c0b98ce65aff20d020dc88746c3a803475e6a
Sarah Vessels <cheshire137@gmail.com>
2016-01-01 12:02:02 -0500

    Highlight unmet step goal in red

--- src/components/FitbitPage/Profile.js
+++ src/components/FitbitPage/Profile.js
@@ -1,11 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import s from './FitbitPage.scss';
+import cx from 'classnames';
 
 class Profile extends Component {
   render() {
     var profileUri = 'https://www.fitbit.com/user/' + this.props.encodedId;
     var hasAvatar = typeof this.props.avatar === 'string' &&
         this.props.avatar.length > 0;
+    var hasSteps = typeof this.props.steps === 'number';
+    var hasGoalSteps = typeof this.props.goalSteps === 'number';
+    var goalClass = hasSteps && hasGoalSteps &&
+        this.props.steps >= this.props.goalSteps ? s.metGoal : s.notMetGoal;
     return (
       <div className={s.profile}>
         {hasAvatar ? (
@@ -14,27 +19,35 @@ class Profile extends Component {
           </a>
         ) : ''}
         <div className={s.fitbitData}>
-          <h3>
-            <a href={profileUri} target="_blank" className={s.fitbitLink}>
-              {this.props.displayName}
-            </a>
-          </h3>
-          {typeof this.props.steps === 'number' ? (
-            <p className={s.steps}>
-              Steps since {this.props.stepCutoffDate}:
-              <span className={s.stepCount}>
-                {this.props.steps.toLocaleString()}
-              </span>
-            </p>
-          ) : ''}
-          {typeof this.props.goalSteps === 'number' ? (
-            <p className={s.goalSteps}>
-              Step goal:
-              <span className={s.stepCount}>
-                {this.props.goalSteps.toLocaleString()}
-              </span>
-            </p>
-          ) : ''}
+          <table>
+            <thead>
+              <tr>
+                <th colSpan="2">
+                  <a href={profileUri} target="_blank" className={s.fitbitLink}>
+                    {this.props.displayName}
+                  </a>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {hasSteps ? (
+                <tr className={s.steps}>
+                  <th>Steps since {this.props.stepCutoffDate}:</th>
+                  <td className={cx(s.stepCount, goalClass)}>
+                    {this.props.steps.toLocaleString()}
+                  </td>
+                </tr>
+              ) : ''}
+              {hasGoalSteps ? (
+                <tr className={s.goalSteps}>
+                  <th>Step goal:</th>
+                  <td className={s.stepCount}>
+                    {this.props.goalSteps.toLocaleString()}
+                  </td>
+                </tr>
+              ) : ''}
+            </tbody>
+          </table>
         </div>
       </div>
     )
